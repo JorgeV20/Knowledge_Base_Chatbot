d792d9c314efc6079e928c2f414b3a7a68b9d1e9
JorgeV20 <jorg.v.2094@gmail.com>
2024-04-22 10:44:17 +0200

    streamlit update

--- st_app.py
+++ st_app.py
@@ -28,7 +28,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.header('Stock Forecast App')
-    #st.image("https://static.streamlit.io/examples/cat.jpg")
+
     START = "2015-01-01"
     TODAY = date.today().strftime("%Y-%m-%d")
     def load_data(ticker):
@@ -57,5 +57,72 @@ with col1:
 
 
 with col2:
-   st.header("Ask to Flint")
-   #st.image("https://static.streamlit.io/examples/dog.jpg")
+    
+    st.header("Ask to Flint")
+
+    DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+    custom_prompt_template = """Use the following pieces of information to answer the user's question.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+    Context: {context}
+    Question: {question}
+
+    Only return the helpful answer below and nothing else. Try to make it short. Maximum of 500 words.
+    Helpful answer:
+    """
+
+    def set_custom_prompt():
+        """
+        Prompt template for QA retrieval for each vectorstore
+        """
+        prompt = PromptTemplate(template=custom_prompt_template,
+                                input_variables=['context', 'question'])
+        return prompt
+
+    #Retrieval QA Chain
+    def retrieval_qa_chain(llm, prompt, db):
+        qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type='stuff',
+                                        retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                        return_source_documents=True,
+                                        chain_type_kwargs={'prompt': prompt}
+                                        )
+        return qa_chain
+
+    #Loading the model
+    def load_llm():
+        # Load the locally downloaded model here
+        llm = CTransformers(
+            model = "TheBloke/Llama-2-7B-Chat-GGML",
+            model_type="llama",
+            max_new_tokens = 512,
+            temperature = 0.5
+        )
+        return llm
+
+    #QA Model Function
+    def qa_bot():
+        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                        model_kwargs={'device': 'cpu'})
+        db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+        llm = load_llm()
+        qa_prompt = set_custom_prompt()
+        qa = retrieval_qa_chain(llm, qa_prompt, db)
+
+        return qa
+
+
+    #output function
+    def final_result(query):
+        qa_result = qa_bot()
+        response = qa_result.invoke({'query': query})
+        return response
+    
+    
+    st.header("Chatbot💁")
+    user_question = st.text_input("Ask a Question from Finance", key="user_question")
+    if user_question:
+        response=final_result(user_question)
+        st.write("Reply: ", response['result'])
+        
