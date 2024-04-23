236ec23bc192d4409c660376d2551c120c1e6c45
JorgeV20 <jorg.v.2094@gmail.com>
2024-04-24 01:36:18 +0200

    streamlit update

--- st_app.py
+++ st_app.py
@@ -18,7 +18,7 @@ import yfinance as yf
 from prophet import Prophet
 import pandas as pd
 from datetime import date
-#from prophet.plot import plot_plotly
+from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 local_path=os.getcwd()
@@ -156,6 +156,8 @@ st.header('Stock Forecast App')
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
+
+@st.cache
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
@@ -176,6 +178,6 @@ st.subheader('Forecast data')
 st.write(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
 
 st.write(f'Forecast plot for 1 year')
-    #fig1 = plot_plotly(m, forecast)
-fig1 = m.plot(forecast)
+fig1 = plot_plotly(m, forecast)
+#fig1 = m.plot(forecast)
 st.plotly_chart(fig1)
