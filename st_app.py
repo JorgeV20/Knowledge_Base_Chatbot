d9cd4f80ede5d25522fd0d08cc0aad9aa92f6f54
JorgeV20 <jorg.v.2094@gmail.com>
2024-04-24 01:42:07 +0200

    streamlit update

--- st_app.py
+++ st_app.py
@@ -157,7 +157,7 @@ st.header('Stock Forecast App')
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-@st.cache
+@st.cache_data
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
