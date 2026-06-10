8feec14313b915b79565665c4dfef61531b8fdc3
JorgeV20 <jorg.v.2094@gmail.com>
2026-06-10 21:42:16 +0200

    modified script

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ from langchain.chains import RetrievalQA
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-#Defining the prompt template
 custom_prompt_template = """Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
@@ -19,54 +18,37 @@ Helpful answer:
 """
 
 def set_custom_prompt():
-    """
-    Prompt template for QA retrieval for each vectorstore
-    """
-    prompt = PromptTemplate(template=custom_prompt_template,
-                            input_variables=['context', 'question'])
-    return prompt
+    return PromptTemplate(template=custom_prompt_template, input_variables=['context', 'question'])
 
-#Retrieval QA Chain
 def retrieval_qa_chain(llm, prompt, db):
-    qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                       chain_type='stuff',
-                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
-                                       return_source_documents=True,
-                                       chain_type_kwargs={'prompt': prompt}
-                                       )
-    return qa_chain
-
-#Loading the model
-def load_llm():
-    # Load the locally downloaded model here
-    llm = CTransformers(
-        model = "TheBloke/Llama-2-7B-Chat-GGML",
-        model_type="llama",
-        max_new_tokens = 512,
-        temperature = 0.5
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type='stuff',
+        retriever=db.as_retriever(search_kwargs={'k': 2}),
+        return_source_documents=True,
+        chain_type_kwargs={'prompt': prompt}
     )
-    return llm
 
-#QA Model Function
-def qa_bot():
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                       model_kwargs={'device': 'cpu'})
-    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
-    llm = load_llm()
-    qa_prompt = set_custom_prompt()
-    qa = retrieval_qa_chain(llm, qa_prompt, db)
+def load_llm():
+    return CTransformers(
+        model="TheBloke/Llama-2-7B-Chat-GGML",
+        model_type="llama",
+        max_new_tokens=512,
+        temperature=0.5
+    )
 
-    return qa
+print("Initializing RAG Models... Please wait (this happens once).")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
+llm = load_llm()
+qa_prompt = set_custom_prompt()
 
+# This is our persistent global chain instance
+QA_BOT_INSTANCE = retrieval_qa_chain(llm, qa_prompt, db)
+print("RAG Pipeline ready!")
+# -------------------------------------------------------------
 
-#output function
 def final_result(query):
-    qa_result = qa_bot()
-    response = qa_result.invoke({'query': query})
+    # Simply invoke the pre-loaded global chain instantly
+    response = QA_BOT_INSTANCE.invoke({'query': query})
     return response
-
-
-#input_query=input('Introduce your query: ')
-#answer=final_result(input_query)
-#We take the 'result' value as the answer of the chatbot
-#print(answer['result'])
